45dc1706745c535e571fe4a09e44393d4e103bc9
nurainwp2 <nurainwp2@gmail.com>
2026-02-10 11:01:57 +0600

    Dropdown shape done

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ let select1 = document.getElementById("sub")
 let studyTxt = document.getElementById("studyTxt")
 let select2 = document.getElementById("level")
 let studyTxt2 = document.getElementById("studyTxt2")
+let studyLink = document.getElementById("study");
+let campusLink = document.getElementById("campus");
+let lifeLink = document.getElementById("life");
+let connectLink = document.getElementById("connect");
+let studyDropdown = document.getElementById("studyDropdown");
+let studyDropdown2 = document.getElementById("studyDropdown2");
+let campusDropdown = document.getElementById("campusDropdown");
+let campusDropdown2 = document.getElementById("campusDropdown2");
+let lifeDropdown = document.getElementById("lifeDropdown");
+let lifeDropdown2 = document.getElementById("lifeDropdown2");
+let connectDropdown = document.getElementById("connectDropdown");
+let connectDropdown2 = document.getElementById("connectDropdown2");
+
+
 
 select1.addEventListener("change", () => {
     if (select1.value === "1") {
@@ -35,3 +49,4 @@ select2.addEventListener("change", () => {
         studyTxt2.innerText = "Post-Graduate"
     }
 })
+
